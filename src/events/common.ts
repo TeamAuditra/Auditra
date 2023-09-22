031150d7682267b81cd01900dc5e86fad65075ae
Rudransh Srivastava <rudranshsrivastava2525@gmail.com>
2023-09-23 02:55:27 +0530

    implemented message analyzer

--- src/events/common.ts
+++ src/events/common.ts
@@ -1,10 +1,24 @@
-import type { ArgsOf, Client } from "discordx";
+import type { ArgsOf } from "discordx";
 import { Discord, On } from "discordx";
+import { EmbedBuilder } from "discord.js";
+import analyzeMessage from "../services/messageAnalyzer.js";
 
 @Discord()
 export class Example {
   @On()
-  messageDelete([message]: ArgsOf<"messageDelete">, client: Client): void {
-    console.log("Message Deleted", client.user?.username, message.content);
+  messageCreate([message]: ArgsOf<"messageCreate">): void {
+    if (message.author.bot) return;
+    analyzeMessage((err: any, data: any) => {
+      if (err) return err;
+      else {
+        const parsedData = JSON.parse(data);
+        const toxicityIndex = parseFloat(parsedData.attributeScores.TOXICITY.summaryScore.value) * 100;
+        if (toxicityIndex >= 40) {
+          message.reply(
+            `message: ${message.content} \n toxicity index: ${toxicityIndex} \n user: ${message.author.username}`
+          );
+        }
+      }
+    }, message.content);
   }
 }
